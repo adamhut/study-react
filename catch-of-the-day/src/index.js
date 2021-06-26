36cd796eb435774a1e482984d4121445d993c1fe
adamhut <adamhut@gmail.com>
2021-06-26 08:27:29 -0700

    wip ep 7

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,8 +2,11 @@ import React from 'react'
 import {render} from 'react-dom'
 
 import StorePicker from './components/StorePicker';
+import App from './components/App';
+import './css/style.css'
 
+// render(<StorePicker/>,document.querySelector('#main'));
 
-render(<StorePicker/>,document.querySelector('#main'));
+render(<App/>,document.querySelector('#main'));
 
 
