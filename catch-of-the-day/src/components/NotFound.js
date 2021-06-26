6735c1e556a865b94163c35cba3bba1656452574
adamhut <adamhut@gmail.com>
2021-06-26 09:40:13 -0700

    wip ep 9

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>Not Found!!</h2>        
+        </div>
+    );    
+}
+
+// class NotFound extends React.Component{
+//     render(){
+//         return (
+//             <h1>Not Found</h1>
+//         );
+//     }
+// }
+
+export default NotFound;
