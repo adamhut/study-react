226cf5372b98824bbb6657e204c2d41d617736d2
adamhut <adamhut@gmail.com>
2021-06-26 07:20:31 -0700

    Initial commit

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,19 @@
+
+
+import React from 'react'
+
+class StorePicker extends React.Component {
+    render(){
+        return (
+            <form action="" className="store-selector">
+                { /** comment */ }
+                <h2> pelase enter a store</h2>
+                <input type="text" required placeholder="store name" />
+                <button type="submit">Visit Store</button>
+            </form>
+        )
+    }
+}
+
+
+export default StorePicker;
