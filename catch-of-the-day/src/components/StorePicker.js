1103d7628510db7b76c9ce4ec4102c7274843229
adamhut <adamhut@gmail.com>
2021-06-26 09:46:59 -0700

    wip  ep 10
    helpers function

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-
+import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
     render(){
@@ -8,7 +8,7 @@ class StorePicker extends React.Component {
             <form action="" className="store-selector">
                 { /** comment */ }
                 <h2> pelase enter a store</h2>
-                <input type="text" required placeholder="store name" />
+                <input type="text" required placeholder="store name"  defaultValue={getFunName()}/>
                 <button type="submit">Visit Store</button>
             </form>
         )
